2cbc45ea547e4d5c5cf3e7169fb72ebb869be4aa
Carlos Portillo <carloskportillo@gmail.com>
2022-08-26 17:31:49 -0600

    Lista de supermercado terminada

--- Guia02/discusion_todo/src/components/Form.jsx
+++ Guia02/discusion_todo/src/components/Form.jsx
@@ -1,24 +1,48 @@
-import React, {useState} from 'react';
+import React, { useState} from 'react';
 import Todo from './Todo';
 
 const Form = () => {
 
-    const [todo, setTodo] = useState({});//Se declara un estado conformado por un solo objeto ({})
-    const [todos, setTodos] = useState([ //Se declara un estado conformado por un array ([]) que contiene objetos ({}).
-        {todo: 'todo1'},
-        {todo: 'todo2'},
-        {todo: 'todo3'},
-        {todo: 'todo4'}
+    const [halfTodo, setMidTodo] = useState(["producto","","cantidad",""]);
+    const [todos, setTodos] = useState([ 
+        {producto: 'Aguacate',
+         cantidad: 2},
+        {producto: 'Papitas',
+         cantidad: 1},
+        {producto: 'Macarrones',
+         cantidad: 3},
+        {producto: 'Cheetos',
+         cantidad: 6}
     ]);
 
-    const handleChange = e => setTodo({[e.target.name]: e.target.value}); //Se crea un trigger de evento, que seteará un objeto, índicandole el nombre (en base al input donde proviene) y valor del mismo.
-    const handleClick = e => {
-        if(Object.keys(todo).length === 0 || todo.todo.trim() === ''){ //Se valida que el input no se encuentre vacío.
+    const handleChange = e => { 
+        const tempTodos = halfTodo;
+        tempTodos[0] = e.target.name;
+        tempTodos[1] = e.target.value;
+        setMidTodo(tempTodos);
+    } 
+
+    const handleCantidad = e => {
+        const tempTodos = halfTodo;
+        tempTodos[2] = e.target.name;
+        tempTodos[3] = e.target.value;
+        setMidTodo(tempTodos);
+    }
+
+    const HandleClick = e => {
+        if(Object.keys(halfTodo).length === 0 || halfTodo[1] === "" || halfTodo[3] === ""){ //Se valida que el input no se encuentre vacío.
             alert('El campo no puede estar vacío');
             return
         }
-        setTodos([...todos, todo]); //Se "sobreescribe" el estado con el array de objetos, con el array de objetos que ya poseía (...) más el nuevo objeto que almacena el otro estado.
+        const toSaveTodos = [...halfTodo];
+        console.log(toSaveTodos);
+        setTodos([...todos,{
+            [toSaveTodos[0]]: [toSaveTodos[1]],
+            [toSaveTodos[2]]: [toSaveTodos[3]]
+        }]);
+        setMidTodo(["producto","","cantidad",""]);
     }
+    
 
     const deleteTodo = (índice) => {
         const newTodos = [...todos]; //Se guardan todos los todos del estado actual en un nuevo array.
@@ -28,17 +52,26 @@ const Form = () => {
 
     return(
         <> {/*preventDefault() sirve para evitar que el navegador se refresque al hacer submit al formulario*/}
-            <form onSubmit={e => e.preventDefault()}> {/*Se crea un formulario que al hacer Submit guarde la "variable" e (la que contiene la información)*/}
+            <form className='toBuyForm' onSubmit={e => e.preventDefault()}> {/*Se crea un formulario que al hacer Submit guarde la "variable" e (la que contiene la información)*/}
                 <label>Agregar tarea</label><br />
-                <input type="text" name='todo' onChange={handleChange} /> {/*Cada que cambie este apartado, se verá actualizado el state*/}
-                <button onClick={handleClick}>Agregar</button> {/*Cuando se le haga click, desencadena el handler anteriormente programado y mostrará en consola el texto debido*/}
+                <input type="text" name='producto' onChange={handleChange}/> {/*Cada que cambie este apartado, se verá actualizado el state*/}
+                <input type="number" name='cantidad' onChange={handleCantidad}/>
+                <button onClick={HandleClick}>Agregar</button> {/*Cuando se le haga click, desencadena el handler anteriormente programado y mostrará en consola el texto debido*/}
             </form>
-           {
-            todos.map((value, index) => (
-                //El JS se escribe entre llaves. Accedemos al estado "todos" y le decimos que lo imprima debido a que es un arreglo, con la ayuda de .map
-                ( <Todo todo={value.todo} key={index} index={index} deleteTodo={deleteTodo} /> ) //También se creó una función anonima con flecha, para acceder a las propiedades de los objetos en el arreglo.
-            ))                                //Se le pasó como "prop" el todo al componente Todo.
-           }
+           <table>
+            <tbody>
+                <tr className='tableHead'>
+                    <th>Producto</th>
+                    <th>Cantidad</th>
+                    <th>Borrar</th>
+                </tr>
+            </tbody>
+            {todos.map((value, index) => (
+                    //El JS se escribe entre llaves. Accedemos al estado "todos" y le decimos que lo imprima debido a que es un arreglo, con la ayuda de .map
+                    ( <Todo todo={value.producto} cantidad={value.cantidad} key={index} index={index} deleteTodo={deleteTodo} /> ) //También se creó una función anonima con flecha, para acceder a las propiedades de los objetos en el arreglo.
+                ))                                //Se le pasó como "prop" el todo al componente Todo.
+            }
+           </table>
         </>
     )
 }
--- Guia02/discusion_todo/src/components/Todo.jsx
+++ Guia02/discusion_todo/src/components/Todo.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 
-const Todo = ({todo, index, deleteTodo}) => { //Estamos haciendo uso de un hook que nos permite acceder al estado en manera de argumento
+const Todo = ({todo,cantidad, index, deleteTodo}) => { //Estamos haciendo uso de un hook que nos permite acceder al estado en manera de argumento
     return(
-        <div className='list'>
-            <h3>{todo}</h3>
-            <button className='btn-delete' onClick={() => deleteTodo(index)}>x</button>
-            {/*Se creó un evento con función anonima que permite activar una función "deleteTodo"  con un argumento 
-            que contendrá la posición del elemento que se quiere borrar en el state con array del componente Form */}
-        </div>
+        <>
+            <tbody>
+                <tr className='tableBody'>
+                    <td>{todo}</td>
+                    <td>{cantidad}</td>
+                    <td> <button className='btn-delete' onClick={() => deleteTodo(index)}>x</button> </td>
+                </tr>
+            </tbody>
+        </>
     )
 }
 
